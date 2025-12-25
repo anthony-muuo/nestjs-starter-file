7352e37e385d5adc61c5bf1b6527f10f1ab40b6a
muuo <anthonymuuo92@gmail.com>
2025-12-25 17:26:40 +0300

    added config module and some ts rules

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
   getHello(): string {
+    //environment is the name of the object property in config file
+    console.log('YOOH', this.configService.get<string>('environment'));
     return 'Hello World!';
   }
 }
